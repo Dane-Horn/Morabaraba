18a3ed3eda63778ad3b722be775a023d205fb57b
g16h0403 <g16h0403@HUG158>
2018-03-01 16:23:55 +0200

    redid board to be dynamic, added some types

--- Morabaraba/Morabaraba/Program.fs
+++ Morabaraba/Morabaraba/Program.fs
@@ -1,45 +1,68 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+
+// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+
+type Piece =
+|X
+|O
+
+type Cell = 
+    {   
+        pos : char*int
+    }
 [<EntryPoint>]
 let main argv = 
-    let boardLayout = "\
-                       a1----------a4----------a7/n/
-                       | '.         |        .' |/n/
-                       |   b2------b4------b6   |/n/
-                       |   | '.     |    .' |   |/n/
-                       |   |   c3--c4--c5   |   |/n/
-                       |   |   |        |   |   |/n/
-                       d1--d2--d3       d5--d6--d7/n/
-                       |   |   |        |   |   |/n/
-                       |   |   e3--e4--e5   |   |/n/
-                       |   | .'     |    '. |   |/n/
-                       |   f2-------f4------f6  |/n/
-                       | .'         |        '. |/n/
-                       g1----------g4----------g7/n"
-    let player1Cows = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12]
-    let player2Cows = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12]
+    let displayBoard (boardString : string) = //takes in a string of cows and places them at the proper positions
+        printf"\t    1   2   3    4   5   6   7 
+        A   %c------------%c-----------%c         Player1 cows 
+        |   | '.         |        .' |         o o o o o o o o o o o o 
+        B   |   %c--------%c-------%c   | 
+        |   |   | '.     |    .' |   |         Player2 cows 
+        C   |   |   %c----%c---%c   |   |         x x x x x x x x x x x x 
+        |   |   |   |        |   |   | 
+        D   %c---%c---%c        %c---%c---%c 
+        |   |   |   |        |   |   | 
+        E   |   |   %c----%c---%c   |   | 
+        |   |   | .'     |    '. |   |
+        F   |   %c--------%c-------%c   |
+        |   | .'         |        '. |
+        G   %c------------%c-----------%c "
+            boardString.[0] boardString.[1] boardString.[2]//row 1
+            boardString.[3] boardString.[4] boardString.[5]//row 2
+            boardString.[6] boardString.[7] boardString.[8]//row 3
+            boardString.[9] boardString.[10] boardString.[11] boardString.[12] boardString.[13] boardString.[14]//row 4
+            boardString.[15] boardString.[16] boardString.[17]//row 5
+            boardString.[18] boardString.[19] boardString.[20]//row 6
+            boardString.[21] boardString.[22] boardString.[23]//row 7
+    
     let possibleMills = [(*horizontal mills*)
-                        "a1, a4, a7";
-                        "b2, b4, b6";
-                        "c3, c4, c5";
-                        "d1, d2, d3";
-                        "d5, d6, d7";
-                        "e3, e4, e5";
-                        "f2, f4, f6";
-                        "g1, g4, g7";
+                        ('A', 1),('A', 4), ('A', 7);
+                        ('B',2), ('B',4), ('B',6);
+                        ('C',3), ('C',4), ('C',5);
+                        ('D',1), ('D',2), ('D',3);
+                        ('D',5), ('D',6), ('D',7);
+                        ('E',3), ('E',4), ('E',5);
+                        ('F',2), ('F',4), ('F',6);
+                        ('G',1), ('G',4), ('G',7);
                         (*vertical mills*)
-                        "a1, d1, g1";
-                        "b2, d2, f2";
-                        "c3, d3, e3";
-                        "a4, b4, c4";
-                        "e4, f4, g4";
-                        "c5, d5, e5";
-                        "b6, d6, f6";
-                        "a7, d7, g7";
+                        ('A',1), ('D',1), ('G',1);
+                        ('B',2), ('D',2), ('F',2);
+                        ('C',3), ('D',3), ('E',3);
+                        ('A',4), ('B',4), ('C',4);
+                        ('E',4), ('F',4), ('G',4);
+                        ('C',5), ('D',5), ('E',5);
+                        ('B',6), ('D',6), ('F',6);
+                        ('A',7), ('D',7), ('G',7);
                         (*diagonal mills*)
-                        "a1, b1, c1";
-                        "a7, b6, c5";
-                        "g1, f2, e3";
-                        "e5, f6, g7";]
+                        ('a',1), ('b',1), ('c',1);
+                        ('a',7), ('b',6), ('c',5);
+                        ('g',1), ('f',2), ('e',3);
+                        ('e',5), ('f',6), ('g',7);]
+
+    
+
+    displayBoard "OOOOOOOOOOOOOOOOOOOOOOOO"
+    Console.ReadLine () |> ignore
     0 // return an integer exit code
