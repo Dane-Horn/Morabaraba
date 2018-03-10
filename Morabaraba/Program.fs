f3f344af1b9b5672b327e0b1c4e34626a827a05e
Dane Horn <dane.horn7915@gmail.com>
2018-03-10 12:05:14 +0200

    Formatting
    Fixed the rules of morabaraba display

--- Morabaraba/Program.fs
+++ Morabaraba/Program.fs
@@ -22,26 +22,27 @@ let displayRules () =
     printfn "
         Morabaraba is a two player game.
         The typical morabaraba board has 24 squares and each player starts with 12 stones (called cows).
-        The first phase of the game involves each player placing one of their cows onto an empty square on the board
+        The first phase of the game involves each player placing one of their cows onto an empty square on the board.
         Placing three of your cows in a straight connected line will result in a mill 
         and allow you to remove or \"shoot\" one of your oppponent's cows
         Cows that are in a mill cannot be shot or removed.
+        If you create a mill and all your opponent's cows are in a mill then a mill is created, but no cow is shot.
         Once both players have placed their twelve stones, the moving phase begins.
         
-        In the moving phase players can move their cows to an adjacent empty square
-        Mills can still be created this way rules for mills and shooting still apply.
+        In the moving phase players can move their cows to an adjacent empty square.
+        Mills can still be created this way and the rules for mills and shooting still apply.
         You cannot move out of and create the same mill with the same piece in consecutive turns,
         if a player does this, the cow returns to the mill, but they cannot shoot an opponent's cow.
         
-        When a player reaches three cows on the board, all their cows become flying cows
+        When a player reaches three cows on the board, all their cows become flying cows.
         A flying cow can move to any empty square on the board rather than just adjacent squares.
         
         End conditions:
-        A player loses when they reach 2 stones in the moving phase
-        A player also loses when they cannot make a move in the moving phase
-        If 10 moves are resolved while both players are on 3 cows then the game ends in a draw
+        A player loses when they reach 2 stones in the moving phase.
+        A player also loses when they cannot make a move in the moving phase.
+        If 10 moves are resolved while both players are on 3 cows then the game ends in a draw.
         If both players place all their cows on the board without forming a mill then 
-        the game ends in a draw as neither player can move"
+        the game ends in a draw as neither player can move."
     printf "Press any key to continue"
     Console.ReadKey () |> ignore
     Console.Clear()
